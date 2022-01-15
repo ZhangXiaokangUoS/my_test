d7a1a979bdf5058cbeb6a092a640dab4d3c15290
ZhangXioakangFire <94939336+ZhangXioakangFire@users.noreply.github.com>
2022-01-15 22:38:08 +0000

    Update test.php

--- test.php
+++ test.php
@@ -5,9 +5,9 @@ $sql = $param_arr["s"];
 echo "sql is: $sql";
 
 $servername = "localhost";
-$username = "maintain";
-$password = "";
-$dbname = "";
+$username = "wordpress";
+$password = "wordpress";
+$dbname = "wordpress";
 
 
 $conn = new mysqli($servername, $username, $password);
